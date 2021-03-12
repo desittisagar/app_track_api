c62a3a4495ec0a151c6d4178db8f08f8bc73c01e
desittisagar <dsagar.smart@gmail.com>
2021-03-12 23:32:29 +0530

    pg-db

--- Gemfile
+++ Gemfile
@@ -45,4 +45,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do
 	gem 'pg'
 end
-#i
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "main#index"		# list of all jobs to non-loggedin users
-  post "apply", to: "apply_job#create"		# to apply for jobs
 
   post "sign_up", to: "registration#create"		# to check valid user and then register
 
@@ -11,6 +10,8 @@ Rails.application.routes.draw do
 
   get "admin_login", to: "session#check_admin"	# check if admin or not - before login with mobile
   post "admin_login", to: "session#login_admin"	# to login admin - mobile and otp
+
+  post "apply", to: "apply_job#create"		# to apply for jobs
 
   get "admin_login/jobs", to: "admin#joblist"	# list of jobs, candidates and applied_jobs
   post "admin_login/jobs/create_jobs", to: "admin#create_job"
