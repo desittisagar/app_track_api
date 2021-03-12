f4c42fd646d62ed7cc36010e71100e153fd7dea2
desittisagar <dsagar.smart@gmail.com>
2021-03-12 12:43:25 +0530

    mysql db changes

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate < ApplicationRecord
 	has_secure_password
 	EMAIL_REGEX = /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/
-	validates :email, presence: true, format: {with: EMAIL_REGEX, message: "Must be a valid email"}
+	validates :email, presence: true, uniqueness: true, format: {with: EMAIL_REGEX, message: "Must be a valid email"}
 	validates :name, presence: true, format: {with: /[a-zA-Z]+/, message: "Must be Characters only"}
 	validates :age, format: {with: /[0-9]+/, message: "Must be integer"}, numericality: { greater_than: 20, less_than_or_equal_to: 40 }
 	validates :yoe, format: {with: /[0-9]+/, message: "Must be integer"}, numericality: { greater_than: 0, less_than_or_equal_to: 20 }
